b1473a5cf3d5ea2cee81405cce90bb9fb8abd570
renjing <renjing@iotsh.com.cn>
2021-03-18 16:37:19 +0800

    删除无用文件

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_authentication.env.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_authentication.env.pro')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Readme
+
+## 项目说明
+
+* 本项目为基于django的认证和权限系统
+* 认证使用 JWT，包含token发放和token refresh，token主动失效
+* 权限包含路由权限和接口权限
+
+### 认证
